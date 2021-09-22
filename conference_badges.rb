49906a70b0a41a8f3a610d69f0d27b13de8735c0
Isabella Mighell <ikm4dm@virginia.edu>
2021-09-21 20:46:14 -0400

    commit tree df2df38e1410880fc85016825226f1139e9d6df3

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator names_arr
+    names_arr.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms names_arr
+    names_arr.map.with_index(1) do |name, count|
+        "Hello, #{name}! You'll be assigned to room #{count}!"
+    end
+end
+
+def printer names_arr
+    batch_badge_creator(names_arr).each do |badge|
+        puts badge
+    end
+    
+    assign_rooms(names_arr).each do |room|
+        puts room
+    end
+end
